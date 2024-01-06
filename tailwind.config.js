07f01119e727f6baa9af18fb1bc79a455b0a6abb
MohamedAdel310 <99290531+MohamedAdelElBaik@users.noreply.github.com>
2024-01-06 18:47:59 +0200

    about-me section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   content: ['./*.{html,js}'],
   theme: {
+    colors: {
+      dark: '#21243D',
+      light: '#EDF7FA',
+      white: '#fff',
+      red: '#FF6464',
+    },
     extend: {},
   },
   plugins: [],
