ecdb12600037322880926c242a0414127dd21966
MohamedAdel310 <99290531+MohamedAdelElBaik@users.noreply.github.com>
2024-01-06 21:58:03 +0200

    create nav and add functionally

---
+++ index.js
@@ -0,0 +1,27 @@
+const navList = document.querySelector('.nav-list');
+const navItems = document.querySelectorAll('.nav-item');
+const navIcon = document.querySelector('.nav-icon');
+
+const hidden = 'hiddenTag';
+const active = 'active';
+
+function addClass(tag, className) {
+  tag.classList.add(className);
+}
+function removeClass(tag, className) {
+  tag.classList.remove(className);
+}
+
+navIcon.addEventListener('click', () => {
+  addClass(navIcon, hidden);
+  removeClass(navList, hidden);
+});
+
+console.log(navItems);
+navItems.forEach((item) => {
+  return item.addEventListener('click', (tag) => {
+    // addClass(tag.target, active);
+    removeClass(navIcon, hidden);
+    addClass(navList, hidden);
+  });
+});
