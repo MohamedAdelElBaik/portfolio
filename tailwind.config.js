29c9915a95254fed72ecb5f02754f04c26308b63
Mohamed Adel <99290531+MohamedAdelElBaik@users.noreply.github.com>
2025-01-09 23:25:52 +0200

    FEAT: sms login page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./*.{html,js}'],
+  content: ["./*.{html,js}"],
   theme: {
     colors: {
-      background: '#0f172a',
-      backgroundDarker: '#0a101e',
-      primary: '#35b4ed',
-      secondary: '#94a3b8',
-      dark: '#21243D',
-      light: '#EDF7FA',
-      white: '#e2e8f0',
-      red: '#FF6464',
+      backgroundDarker: "#0a101e",
+      primary: "hsl(var(--primary))",
+      primaryMuted: "hsl(var(--primary-muted))",
+      secondary: "hsl(var(--secondary))",
+      dark: "#21243D",
+      light: "#EDF7FA",
+      white: "#e2e8f0",
+      red: "#FF6464",
     },
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "radial-gradient-custom":
+          "radial-gradient(circle, rgba(169,37,181,1) 0%, rgba(119,22,128,1) 100%)",
+      },
+    },
   },
   plugins: [],
 };
