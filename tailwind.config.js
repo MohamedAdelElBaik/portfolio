31a3226b09c15ce62720ad289bc5d81ff8f3c82c
Mohamed Adel <99290531+MohamedAdelElBaik@users.noreply.github.com>
2025-01-10 01:01:00 +0200

    FEAT: create dashboard layout page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,17 +2,17 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    colors: {
-      backgroundDarker: "#0a101e",
-      primary: "hsl(var(--primary))",
-      primaryMuted: "hsl(var(--primary-muted))",
-      secondary: "hsl(var(--secondary))",
-      dark: "#21243D",
-      light: "#EDF7FA",
-      white: "#e2e8f0",
-      red: "#FF6464",
-    },
     extend: {
+      colors: {
+        backgroundDarker: "#0a101e",
+        primary: "hsl(var(--primary))",
+        primaryMuted: "hsl(var(--primary-muted))",
+        secondary: "hsl(var(--secondary))",
+        dark: "#21243D",
+        light: "#EDF7FA",
+        white: "#e2e8f0",
+        red: "#FF6464",
+      },
       backgroundImage: {
         "radial-gradient-custom":
           "radial-gradient(circle, rgba(169,37,181,1) 0%, rgba(119,22,128,1) 100%)",
